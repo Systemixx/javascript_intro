1df9c5de7ce24f6c6839ffda2da4ae2780667a09
Chris Neidl <chris.neidl@gmx.de>
2024-03-15 16:40:47 +0100

    erstellen

---
+++ HA-15.03.2024.js
@@ -0,0 +1,81 @@
+// 1. Lieblingsbuch
+let lieblingsBuch = "Der kleine Prinz";
+
+// 2. Anzahl Geschwister
+let anzahlGeschwister = 3;
+
+// 3. Hat Führerschein
+let hatFuehrerschein = true;
+
+// 4. Momentane Stimmung
+let momentaneStimmung = null;
+
+// 5. Nächstes Reiseziel
+let naechsteReiseziel;
+
+// 6. Lieblingsfarbe
+let lieblingsFarbe = "Blau";
+
+// 7. Wohnfläche
+let wohnflaeche = 380;
+
+// 8. Mag Kaffee
+let magKaffee = false;
+
+// 9. Aktuelles Projekt
+let aktuellesProjekt = null;
+
+// 10. Letztes Abendessen
+let letztesAbendessen;
+
+// 11. Geburtsstadt
+let geburtsstadt = "Nabburg";
+
+// 12. Kaffeetassen pro Tag
+let kaffeetassenProTag = 0;
+
+// 13. Ist Vegetarier
+let istVegetarier = false;
+
+// 14. Zukunftswunsch
+let zukunftsWunsch = null;
+
+// 15. Lieblings-Sportart
+let lieblingsSportart;
+
+// 16. Lieblingsfilm
+let lieblingsFilm = "Inception";
+
+// 17. Höhe in cm
+let hoeheInCm = 175;
+
+// 18. Hat Haustier
+let hatHaustier = false;
+
+// 19. Mittagessen Idee
+let mittagessenIdee = null;
+
+// 20. Lieblingsmusikband
+let lieblingsMusikband;
+
+// Ausgabe in der Konsole
+console.log("1. Lieblingsbuch:", lieblingsBuch, typeof lieblingsBuch);
+console.log("2. Anzahl Geschwister:", anzahlGeschwister, typeof anzahlGeschwister);
+console.log("3. Hat Führerschein:", hatFuehrerschein, typeof hatFuehrerschein);
+console.log("4. Momentane Stimmung:", momentaneStimmung, typeof momentaneStimmung);
+console.log("5. Nächstes Reiseziel:", naechsteReiseziel, typeof naechsteReiseziel);
+console.log("6. Lieblingsfarbe:", lieblingsFarbe, typeof lieblingsFarbe);
+console.log("7. Wohnfläche:", wohnflaeche, typeof wohnflaeche);
+console.log("8. Mag Kaffee:", magKaffee, typeof magKaffee);
+console.log("9. Aktuelles Projekt:", aktuellesProjekt, typeof aktuellesProjekt);
+console.log("10. Letztes Abendessen:", letztesAbendessen, typeof letztesAbendessen);
+console.log("11. Geburtsstadt:", geburtsstadt, typeof geburtsstadt);
+console.log("12. Kaffeetassen pro Tag:", kaffeetassenProTag, typeof kaffeetassenProTag);
+console.log("13. Ist Vegetarier:", istVegetarier, typeof istVegetarier);
+console.log("14. Zukunftswunsch:", zukunftsWunsch, typeof zukunftsWunsch);
+console.log("15. Lieblings-Sportart:", lieblingsSportart, typeof lieblingsSportart);
+console.log("16. Lieblingsfilm:", lieblingsFilm, typeof lieblingsFilm);
+console.log("17. Höhe in cm:", hoeheInCm, typeof hoeheInCm);
+console.log("18. Hat Haustier:", hatHaustier, typeof hatHaustier);
+console.log("19. Mittagessen Idee:", mittagessenIdee, typeof mittagessenIdee);
+console.log("20. Lieblingsmusikband:", lieblingsMusikband, typeof lieblingsMusikband);
