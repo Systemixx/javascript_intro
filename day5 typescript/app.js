057451b1ccf61c028bc29942dcf7349f4d1e6687
Chris Neidl <chris.neidl@gmx.de>
2024-03-21 13:00:54 +0100

    update typescriipt

---
+++ day5 typescript/app.js
@@ -0,0 +1,14 @@
+console.log("Heute ist Donnerstag");
+console.log("tsc -w app.ts");
+// Variablen deklarieren
+var trainer = 'Lucas';
+var tag;
+// var ist böse
+// var getränk = 'Kaffe'
+tag = 'Freitag';
+// tag = 4
+console.log(tag);
+function verdoppeln(zahl) {
+    return zahl * 2;
+}
+console.log(verdoppeln(50));
---
+++ day5 typescript/app.ts
@@ -0,0 +1,21 @@
+console.log("Heute ist Donnerstag")
+console.log("tsc -w app.ts") 
+
+// Variablen deklarieren
+
+const trainer = 'Lucas'
+let tag: string
+
+// var ist böse
+// var getränk = 'Kaffe'
+
+tag = 'Freitag'
+// tag = 4
+
+console.log(tag)
+
+function verdoppeln(zahl) {
+    return zahl * 2
+}
+
+console.log(verdoppeln(50))
